4b03524c9691f05f0b8026a5557108ba5da45495
feychou <federica.recanatini@gmail.com>
2020-08-29 14:23:32 +0200

    routes and controllers

--- server.js
+++ server.js
@@ -1,57 +1,16 @@
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-  {id: 1, text: 'todo 1'},
-  {id: 2, text: 'todo 2'},
-  {id: 3, text: 'todo 3'}
-];
-
-const server = http.createServer((req, res) => {
-  //res.setHeader('Content-Type', 'application/json');
-  //res.setHeader('X-Powered-By', 'Node.js');
-  //console.log(req.headers.authorization); access auth token from the request
-  const { method, url } = req;
-  let body = [];
-
-  req
-    .on('data', chunk => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      let status = 404;
-      const response = {
-        success: false,
-        data: null
-      };
-
-      if(method === 'GET' && url === '/todos') {
-        status = 200;
-        response.success = true;
-        response.data = todos;
-      } else if (method === 'POST' && url === '/todos') {
-        const {id, text} = JSON.parse(body);
-
-        if (!id || !text) {
-          status = 400
-          response.error = "please be decent"
-        } else {
-          todos.push({id, text});
-          status = 201;
-          response.success = true;
-          response.data = todos;         
-        }
-      }
-
-      res.writeHead(status, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-      });
+dotenv.config({ path: './config/config.env' });
+const bootcamps = require('./routes/bootcamps');
 
 
-      res.end(JSON.stringify(response));
-    });
+const { PORT, NODE_ENV } = process.env;
+const port = PORT || 5000;
+const ROOT = '/api/v1'
 
-});
+const app = express();
+app.use(`${ROOT}/bootcamps`, bootcamps);
 
-server.listen(5000, () => console.log(`Server running on port 5000`));
+
+app.listen(port, console.log(`Server running in ${NODE_ENV} mode on port ${port}`));

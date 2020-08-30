9a78bed92c3d60354164ba00ee7eb7ed553dfafd
feychou <federica.recanatini@gmail.com>
2020-08-30 11:15:22 +0200

    Add model and CRUD routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,39 +1,78 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200);
-  res.json({success: true, msg: "show all bootcamps"});
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200);
+    res.json({success: true, msg: "show all bootcamps", data: bootcamps});
+  } catch (err) {
+    res.status(500).json({ success: false });
+  }
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200);
-  res.json({success: true, msg: `display bootcamp ${req.params.id}`});
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const bootcamp = await Bootcamp.findById(id);
+    if (!bootcamp) {
+      return res.status(404).json({ success: false })
+    }
+    res.status(200);
+    res.json({success: true, msg: `display bootcamp ${id}`, data: bootcamp});
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps/
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200);
-  res.json({success: true, msg: "create new bootcamp"});
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201);
+    res.json({success: true, data: bootcamp, msg: "create new bootcamp"});
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-  res.status(200);
-  res.json({success: true, msg: `update bootcamp ${req.params.id}`});
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {new: true, runValidators: true});
+    if (!bootcamp) {
+      return res.status(404).json({ success: false })
+    }    
+    res.status(200);
+    res.json({success: true, msg: `update bootcamp ${id}`, data: bootcamp});
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Delete bootcamp
 // @route   DELECT /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(200);
-  res.json({success: true, msg: `delete bootcamp ${req.params.id}`});
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const bootcamp = await Bootcamp.findByIdAndDelete(id);
+    if (!bootcamp) {
+      return res.status(404).json({ success: false })
+    }
+    res.status(200);
+    res.json({success: true, msg: `delete bootcamp ${req.params.id}`});
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 }
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config();
+
+connectDB();
 const bootcamps = require('./routes/bootcamps');
 
 
@@ -10,7 +15,18 @@ const port = PORT || 5000;
 const ROOT = '/api/v1'
 
 const app = express();
+app.use(express.json());
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));  
+}
 app.use(`${ROOT}/bootcamps`, bootcamps);
 
 
-app.listen(port, console.log(`Server running in ${NODE_ENV} mode on port ${port}`));
+const server = app.listen(port, console.log(`Server running in ${NODE_ENV} mode on port ${port}`.cyan.bold));
+
+// unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err}`.red.bold);
+  server.close(() => process.exit(1))
+});
